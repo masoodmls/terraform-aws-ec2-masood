cce8dcfdfabda5afa47e9fc6bd74f6c1d0426f44
Masood Ahmed <masood@gmail.com>
2024-05-29 01:17:44 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,7 @@
+provider "aws" {
+}
+
+resource "aws_instance" "example" {
+  ami           = var.ami_value
+  instance_type = var.instance_type_value
+}
---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+ami_value           = "ami-0cc9838aa7ab1dce7"
+instance_type_value = "t2.micro"
